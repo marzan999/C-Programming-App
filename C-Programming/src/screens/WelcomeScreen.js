ac2ae8e22fb76c3e5e653bdcf0e938abf22dc59a
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-02-18 00:59:07 +0600

    animation add

--- C-Programming/src/screens/WelcomeScreen.js
+++ C-Programming/src/screens/WelcomeScreen.js
@@ -3,53 +3,47 @@ import { View, Pressable, StyleSheet, Dimensions, Image, ImageBackground, Status
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Text from '../components/text/text'
 import { colors } from '../theme/color'
+import LottieView from 'lottie-react-native';
 
 export default function WelcomeScreen({ navigation }) {
     return (
-        <SafeAreaView style={{ backgroundColor: '#193739', flex: 1, }}>
-
-            <View style={styles.parent}>
-                {/* <View style={styles.viewStyle}>
+        <SafeAreaView style={{ backgroundColor: '#193739', flex: 1 }}>
+                <StatusBar barStyle="light-content"/>
+                <View style={styles.viewStyle}>
                     <Text preset="h9" style={styles.cStyle}>C</Text>
                     <Text style={styles.programmingStyle}>PROGRAMMING</Text>
-                </View> */}
+                </View>
                 {/* <Image  source={require('../../assets/wc.jpg')} style={{ alignSelf: 'center', width: Dimensions.get('screen').width, height: Dimensions.get('screen').height}} /> */}
 
-                
-                <ImageBackground source={require('../../assets/wc.jpg')} style={{ alignSelf: 'center', width: Dimensions.get('screen').width, height: Dimensions.get('screen').height, alignItems: 'center',justifyContent: 'center'}}>
-                <Image />
+                <LottieView style={styles.lottie} source={require('../../assets/pro.json')} autoPlay loop />
+                {/* <ImageBackground source={require('../../assets/wc.jpg')} style={{ alignSelf: 'center', width: Dimensions.get('screen').width, height: Dimensions.get('screen').height, alignItems: 'center'}}> */}
                 <Pressable style={styles.btn} onPress={() => navigation.navigate("Home")}>
                     <Text style={styles.getStartedStyle}>Get Started</Text>
                 </Pressable>
-                </ImageBackground>
-            </View>
-            <StatusBar barStyle="light-content"/>
+                {/* </ImageBackground> */}
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    // cStyle: {
-    //     paddingTop: 140,
-    //     paddingLeft: 100,
-    //     // paddingLeft: Dimensions.get('screen').paddingLeft, 
-    //     // width: Dimensions.get('screen').width - 100, 
-    //     // height: Dimensions.get('screen').height
+    cStyle: {
+        paddingTop: 60,
+        paddingLeft: 100,
+        // paddingLeft: Dimensions.get('screen').paddingLeft, 
+        // width: Dimensions.get('screen').width - 100, 
+        // height: Dimensions.get('screen').height
 
-    // },
-    parent: {
-        alignItems: 'center',
     },
 
-    // programmingStyle: {
-    //     paddingTop: 294,
-    //     fontSize: 30,
-    //     marginLeft: -80,
-    //     fontWeight: 'bold',
-    //     // width: Dimensions.get('screen').width , 
-    //     // height: Dimensions.get('screen').height
+    programmingStyle: {
+        paddingTop: 213,
+        fontSize: 30,
+        marginLeft: -80,
+        fontWeight: 'bold',
+        // width: Dimensions.get('screen').width , 
+        // height: Dimensions.get('screen').height
 
-    // },
+    },
 
     viewStyle: {
         flexDirection: 'row',
@@ -58,12 +52,21 @@ const styles = StyleSheet.create({
     getStartedStyle: {
         fontWeight: 'bold',
         fontSize: 35,
-        padding: 15,
+        paddingHorizontal: 15,
+        paddingVertical: 10
     },
     btn:{
-        borderWidth: 1,
+        borderWidth: 3,
         borderColor: 'white',
         borderRadius: 40,
-        marginTop: 400,
+        position: 'absolute',
+        bottom: 130,
+        alignSelf: 'center',
+    },
+    lottie: {
+        height: 350, 
+        width: 350,
+        marginTop: -25,
+        alignSelf: 'center',
     }
 });
